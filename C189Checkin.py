78273b823858ebde1aeaef1dd7e3c1ff00e27baa
liphily <33564466+liphily@users.noreply.github.com>
2020-08-27 10:33:39 +0800

    Update C189Checkin.py

--- C189Checkin.py
+++ C189Checkin.py
@@ -1,8 +1,8 @@
 import requests, time
 
 def main():
-    gonghao='{{ secrets.GH }}'
-    shoujihao='{{ secrets.SJH }}'
+    gonghao='${{ secrets.GH }}'
+    shoujihao='${{ secrets.SJH }}'
     url="http://xg.sqxy.edu.cn/xgh5/openData"
     r = requests.Session()
     riqi=time.strftime('%Y-%m-%d',time.localtime(time.time())) #年-月-日   格式的当天日期
